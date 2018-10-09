0eb2138eecf4ee3a1229cb5e5f7a982dd3f9d887
Lee <lnevo@usa.net>
2018-10-09 01:18:43 -0400

    Some cleanup

--- src/public/radb/config_template.php
+++ src/public/radb/config_template.php
@@ -1,24 +1,20 @@
 <?php
   # Controller URL
   $GLOBALS['RAURL']="http://YOUR.RA.IP:2000/";
-  $GLOBALS['RAURL2']="http://YOUR.RA.IP:2010/";
   # Labels
 
   # External URLS
   $GLOBALS['RACAM']="http://YOUR.WEBCAM.IP:8080/image.jpg?cidx=584511813&timestamp=";
   $GLOBALS['RACAM2']="http://YOUR.WEBCAM.IP:8090/image/jpeg.cgi";
-  $GLOBALS['LabelURL']="http://forum.reefangel.com/status/labels.aspx?id=RAFORUM_USERNAME";
-  $GLOBALS['LabelURL2']="http://forum.reefangel.com/status/labels.aspx?id=RAFORUM_USERNAME2";
-  $GLOBALS['RTRoot']="http://www.reeftronics.net/PREFIX/USERNAME";
 
   # Data Files
-  $GLOBALS['WEBROOT']="http://YOUR.WEB.IP";
-  $GLOBALS['LabelURL']=$GLOBALS['WEBROOT'] . "/radb/data/ra_labels.txt";
-  $GLOBALS['LabelURL2']=$GLOBALS['WEBROOT'] . "/radb/data/ra2_labels.txt";
-  $GLOBALS['R99cache']=$GLOBALS['WEBROOT'] . "/radb/data/r99.txt";
-  $GLOBALS['R99cache2']=$GLOBALS['WEBROOT'] . "/radb/data/r99_2.txt";
-  $GLOBALS['CustomMemCache']=$GLOBALS['WEBROOT'] . "/radb/data/mr_custom.txt";
-  $GLOBALS['FullMemCache']=$GLOBALS['WEBROOT'] . "/radb/data/mr_full_mem.txt";
-  $GLOBALS['AllCache']=$GLOBALS['WEBROOT'] . "/radb/data/all_data.txt";
-  $GLOBALS['MRTG']=$GLOBALS['WEBROOT'] . "/mrtg";
+  $GLOBALS['WEBROOT']="http://YOUR.WEB.SERVER/";
+  $GLOBALS['RADB']=$GLOBALS['WEBROOT'] . "radb/";
+  $GLOBALS['MRTG']=$GLOBALS['WEBROOT'] . "mrtg/";
+  $GLOBALS['RADB_DATA']=$GLOBALS['RADB'] . "data/";
+  $GLOBALS['LabelURL']=$GLOBALS['RADB_DATA'] . "ra_labels.txt";
+  $GLOBALS['R99cache']=$GLOBALS['RADB_DATA'] . "r99.txt";
+  $GLOBALS['CustomMemCache']=$GLOBALS['RADB_DATA'] . "mr_custom.txt";
+  $GLOBALS['FullMemCache']=$GLOBALS['RADB_DATA'] . "mr_full_mem.txt";
+  $GLOBALS['AllCache']=$GLOBALS['RADB_DATA'] . "all_data.txt";
 ?>
